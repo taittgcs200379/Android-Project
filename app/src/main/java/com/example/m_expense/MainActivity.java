e1bdb16eda760397d301f8b8cd6b40152e2b9bdd
taittgcs200379 <taittgcs200379@fpt.edu.vn>
2023-09-19 15:34:58 +0700

    update for edit detail

--- app/src/main/java/com/example/m_expense/MainActivity.java
+++ app/src/main/java/com/example/m_expense/MainActivity.java
@@ -1,105 +1,88 @@
 package com.example.m_expense;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.text.Editable;
 import android.text.TextUtils;
-import android.text.TextWatcher;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
-import android.widget.DatePicker;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.Spinner;
 import android.widget.Toast;
 
-import java.util.Calendar;
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String[] Length={"1000", "2000", "3000"};
+    private static final String[] Level={"beginner", "professional"};
 
-    Date date;
+    AutoCompleteTextView date;
+    Button btDate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        AutoCompleteTextView pLength= (AutoCompleteTextView)findViewById(R.id.length);
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line,Length);
+        //dataAdapter.setDropDownViewResource((android.R.layout.simple_dropdown_item_1line));
+        pLength.setAdapter(dataAdapter);
+        pLength.setThreshold(256);
 
-        EditText date;
-
-        date = (EditText)findViewById(R.id.date);
-        date.addTextChangedListener(new TextWatcher() {
-            private String current = "";
-            private String ddmmyyyy = "DDMMYYYY";
-            private Calendar cal = Calendar.getInstance();
-
-
+        findViewById(R.id.length).setOnClickListener(new OnClickListener(){
             @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (!s.toString().equals(current)) {
-                    String clean = s.toString().replaceAll("[^\\d.]", "");
-                    String cleanC = current.replaceAll("[^\\d.]", "");
-
-                    int cl = clean.length();
-                    int sel = cl;
-                    for (int i = 2; i <= cl && i < 6; i += 2) {
-                        sel++;
-                    }
-                    //Fix for pressing delete next to a forward slash
-                    if (clean.equals(cleanC)) sel--;
-
-                    if (clean.length() < 8){
-                        clean = clean + ddmmyyyy.substring(clean.length());
-                    }else{
-                        //This part makes sure that when we finish entering numbers
-                        //the date is correct, fixing it otherwise
-                        int day  = Integer.parseInt(clean.substring(0,2));
-                        int mon  = Integer.parseInt(clean.substring(2,4));
-                        int year = Integer.parseInt(clean.substring(4,8));
-
-                        if(mon > 12) mon = 12;
-                        cal.set(Calendar.MONTH, mon-1);
-                        if(mon<1) mon=1;
-                        cal.set(Calendar.MONTH, mon+1);
-                        if(day>31) day=31;
-                        cal.set(Calendar.DAY_OF_MONTH, day-1);
-                        if(day<1) day=1;
-                        cal.set(Calendar.DAY_OF_MONTH, day-1);
-                        if
-
-                        year = (year<1900)?1900:(year>2100)?2100:year;
-                        cal.set(Calendar.YEAR, year);
-                        // ^ first set year for the line below to work correctly
-                        //with leap years - otherwise, date e.g. 29/02/2012
-                        //would be automatically corrected to 28/02/2012
-
-                        day = (day > cal.getActualMaximum(Calendar.DATE))? cal.getActualMaximum(Calendar.DATE):day;
-                        clean = String.format("%02d%02d%02d",day, mon, year);
-                    }
-
-                    clean = String.format("%s/%s/%s", clean.substring(0, 2),
-                            clean.substring(2, 4),
-                            clean.substring(4, 8));
-
-                    sel = sel < 0 ? 0 : sel;
-                    current = clean;
-                    date.setText(current);
-                    date.setSelection(sel < current.length() ? sel : current.length());
-
-                }
+            public void onClick(View view){
+                pLength.showDropDown();
             }
+        });
 
 
+        AutoCompleteTextView pLevel= (AutoCompleteTextView)findViewById(R.id.level);
+        ArrayAdapter<String> levelAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line,Level);
+        //dataAdapter.setDropDownViewResource((android.R.layout.simple_dropdown_item_1line));
+        pLevel.setAdapter(levelAdapter);
+        pLevel.setThreshold(256);
+
+        findViewById(R.id.level).setOnClickListener(new OnClickListener(){
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            public void onClick(View view){
+                pLevel.showDropDown();
+            }
+        });
 
+
+        date=findViewById(R.id.date);
+        btDate=findViewById(R.id.date_button);
+
+        btDate.setOnClickListener(new OnClickListener() {
             @Override
-            public void afterTextChanged(Editable s) {}
+            public void onClick(View view) {
+                showDatePickerDialog();
+            }
         });
 
     }
+
+    private void showDatePickerDialog() {
+        MaterialDatePicker materialDatePicker=MaterialDatePicker.Builder.datePicker().build();
+
+        materialDatePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener() {
+            @Override
+            public void onPositiveButtonClick(Object selection) {
+                date.setText(""+materialDatePicker.getHeaderText());
+
+            }
+        });
+        materialDatePicker.show(getSupportFragmentManager(),"TAG");
+
+    }
+
     public void handleButtonClick(View v){
         EditText editText2= (EditText)findViewById(R.id.TripName);
 
@@ -115,12 +98,25 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
-        EditText d = (EditText) findViewById(R.id.date);
-        if(TextUtils.isDigitsOnly(d.getText().toString())){
+        AutoCompleteTextView d = (AutoCompleteTextView) findViewById(R.id.date);
+        if(TextUtils.isEmpty(d.getText().toString())){
             Toast t= Toast.makeText(this, "you must choose date", Toast.LENGTH_SHORT);
             t.show();
             return;
         }
+        AutoCompleteTextView l =(AutoCompleteTextView)findViewById((R.id.length));
+        if(TextUtils.isEmpty(l.getText().toString())){
+            Toast t= Toast.makeText(this, "you must choose length", Toast.LENGTH_SHORT);
+            t.show();
+            return;
+        }
+        AutoCompleteTextView level =(AutoCompleteTextView)findViewById((R.id.level));
+        if(TextUtils.isEmpty(level.getText().toString())){
+            Toast t= Toast.makeText(this, "you must choose level", Toast.LENGTH_SHORT);
+            t.show();
+            return;
+        }
+
         RadioGroup rg=(RadioGroup) findViewById(R.id.radioGroup2);
         if(rg.getCheckedRadioButtonId()==-1){
             Toast t= Toast.makeText(this, "you must choose your option", Toast.LENGTH_SHORT);
